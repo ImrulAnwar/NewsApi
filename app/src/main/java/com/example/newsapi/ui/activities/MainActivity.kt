82d3c0ab9beafac2c29a56a44adcabc5f2131c84
Imrul <imrulanwar836@gmail.com>
2022-05-04 22:52:42 +0600

    Changed the UI Theme

--- app/src/main/java/com/example/newsapi/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/ui/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.newsapi.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapi.R
 import com.example.newsapi.db.Entities.Article
@@ -20,6 +23,15 @@ class MainActivity : AppCompatActivity() {
                 val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
                 val navController = navHostFragment.navController
                 bottomNavigationView.setupWithNavController(navController)
+
+                val appBarConfiguration = AppBarConfiguration(
+                        setOf(
+                                R.id.savedNewsFragment,
+                                R.id.searchNewsFragment,
+                                R.id.breakingNewsFragment
+                        )
+                )
+                setupActionBarWithNavController(navController, appBarConfiguration)
 
                 mViewModel = ViewModelProvider(this)[ArticleViewModel::class.java]
         }
